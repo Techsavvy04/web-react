43f3952f43031c767005b5aef89ab67afb660d3d
Techsavy04 <tna121104@gmail.com>
2024-02-01 16:22:29 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Route,Router,Routes} from 'react-router-dom'
+const Title =  Typography;
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Chatroom/index.js
@@ -0,0 +1,13 @@
+import {Row,Col,Typography} from "antd"
+const Title =Typography;
+function Home(){
+    return(
+        <div>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </div>
+    )
+}
